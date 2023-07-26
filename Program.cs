c2cac18a68132f856b29b33c9e6b59917c3c4b2a
ZafarUrakov <goliburakov535@gmail.com>
2023-07-25 18:25:06 -0700

    Fixed Bags

--- Program.cs
+++ Program.cs
@@ -6,48 +6,44 @@ namespace Averagely
     {
         static void Main()
         {
-
-            Console.WriteLine("\tHi student,please enter your grades for this year.\n");
-
-            Console.Write("English: ");
-            double english = double.Parse(Console.ReadLine()!);
-
-            Console.Write("Mathematics: ");
-            double math = double.Parse(Console.ReadLine()!);
-
-            Console.Write("Geography: ");
-            double geography = double.Parse(Console.ReadLine()!);
-
-            Console.Write("Biology: ");
-            double biology = double.Parse(Console.ReadLine()!);
-
-            Console.Write("Chemistry: ");
-            double chemistry = double.Parse(Console.ReadLine()!);
-
-            double allBall = english + math + geography + biology + chemistry;
-            double answer = allBall / 5;
-            Console.WriteLine($"\nYour avarege score = {answer}!\n");
-
-            switch(answer)
+            Console.WriteLine("How old are you student?: ");
+            Console.Write("Old: ");
+            int old = Convert.ToInt32(Console.ReadLine()!);
+            if(old < 25)
             {
-                case >= 4:
-                    Console.WriteLine("You're doing great!");
-                    break;
-                default:
-                    Console.WriteLine("You need to do better next year , apprentice!");
-                    break;
-            }
-            if(answer <= 5 && answer >=4)
-            {
-                Console.WriteLine("You're doing great!");
-            }
-            else if (answer < 4 && answer >= 3)
-            {
-                Console.WriteLine("Not bad!");
+                Console.WriteLine("\tHi student,please enter your grades for this year.\n");
+
+                Console.Write("English: ");
+                double english = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Mathematics: ");
+                double math = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Geography: ");
+                double geography = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Biology: ");
+                double biology = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Chemistry: ");
+                double chemistry = double.Parse(Console.ReadLine()!);
+
+                double allBall = english + math + geography + biology + chemistry;
+                double answer = allBall / 5;
+                Console.WriteLine($"\nYour avarege score = {answer}!\n");
+
+                switch(answer)
+                {
+                    case >= 4:
+                        Console.WriteLine("You're doing great!");
+                        break;
+                    default:
+                        Console.WriteLine("You need to do better next year , apprentice!");
+                        break;
+                }
             }
             else
-                Console.WriteLine("You need to do better next year , apprentice!");
-
+                Console.WriteLine("I am sorry , but you did not study with us...");
         }
     }
 }
